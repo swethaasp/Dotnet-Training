720681454add485f93fb7d33c0f99ffaaaa799b9
Swethaa S P <97969029+swethaasp@users.noreply.github.com>
2024-08-24 22:42:59 +0530

    Create comaprison.cs

---
+++ M1/Basics/operators/comaprison.cs
@@ -0,0 +1,24 @@
+using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+namespace Basics
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {            int a = 50;
+            Console.WriteLine($"a == 50 : {a == 50}");
+            Console.WriteLine($"a!=50 : {a != 50}");
+            Console.WriteLine($"a > 50 : {a  > 50}");
+            Console.WriteLine($"a < 50 : {a < 50}");
+
+        }
+    }
+}
+a == 50 : True
+a!=50 : False
+a > 50 : False
+a < 50 : False
