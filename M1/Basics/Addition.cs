13a84b663d108032f03383f009a3f660e4ea3739
Swethaa S P <97969029+swethaasp@users.noreply.github.com>
2024-08-24 20:31:17 +0530

    Create Addition.cs

---
+++ M1/Basics/Addition.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Basics    //indicates path -project name (program.cs is inside Basics project)
+{
+   class Program
+    {
+       
+        static void Main(string[] args)
+        {
+
+            int a = 122;
+            int b = 038;
+            int sum = a + b;
+            Console.WriteLine("Addition is : {0} ", sum);  //{0} is replaced by sum
+            Console.WriteLine("Hi Swethaa!");
+            Console.WriteLine("How are You");
+
+
+
+        }
+    }
+}
