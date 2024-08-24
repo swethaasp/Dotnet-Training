14605dc4af770efe5eb944456acc86afe6b6ee93
Swethaa S P <97969029+swethaasp@users.noreply.github.com>
2024-08-24 19:39:11 +0530

    Create structure.cs

---
+++ M1/Basics/structure.cs
@@ -0,0 +1,16 @@
+namespace Basics    //indicates path -project name (program.cs is inside Basics project)
+{
+   class Program
+    {
+       
+        static void Main(string[] args)
+        {
+           
+
+            Console.WriteLine("Hi Swethaa!");
+
+
+
+        }
+    }
+}
