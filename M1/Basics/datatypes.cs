d0b9355163ec5c012213972ec8401341b3629d7a
Swethaa S P <97969029+swethaasp@users.noreply.github.com>
2024-08-24 19:37:31 +0530

    Create datatypes.cs

---
+++ M1/Basics/datatypes.cs
@@ -0,0 +1,23 @@
+namespace Basics    //indicates path -project name (program.cs is inside Basics project)
+{
+   class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+            int c = a + b;
+
+            float d = 12.3F;
+
+            ulong x = 12356467829928376454UL;
+
+            Console.WriteLine("The float value is " + x);  //concatenation
+
+
+
+        }
+    }
+}
+
