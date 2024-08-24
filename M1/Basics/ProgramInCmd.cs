0649cf0f04e4a8875f613f572ec21216785c12e2
Swethaa S P <97969029+swethaasp@users.noreply.github.com>
2024-08-24 20:10:44 +0530

    Create ProgramInCmd.cs

---
+++ M1/Basics/ProgramInCmd.cs
@@ -0,0 +1,19 @@
+**********************************************************************
+** Visual Studio 2022 Developer Command Prompt v17.11.1
+** Copyright (c) 2022 Microsoft Corporation
+**********************************************************************
+
+C:\Program Files\Microsoft Visual Studio\2022\Community>cd/    //put cd/
+
+C:\>cd Users\SWETHAA\source\repos\Solution\Basics    //paste the path
+
+
+C:\Users\SWETHAA\source\repos\Solution\Basics>csc Program.cs   //compile the code
+Microsoft (R) Visual C# Compiler version 4.11.0-3.24376.4 (5e3a11e2)
+Copyright (C) Microsoft Corporation. All rights reserved.
+
+
+C:\Users\SWETHAA\source\repos\Solution\Basics>Program   //run the code
+Hi Swethaa!
+
+  
