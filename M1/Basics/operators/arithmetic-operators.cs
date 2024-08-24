fe806dcc6c674558c26a35ab1cc510ec4966d3a7
Swethaa S P <97969029+swethaasp@users.noreply.github.com>
2024-08-24 22:28:37 +0530

    Create arithmetic-operators.cs

---
+++ M1/Basics/operators/arithmetic-operators.cs
@@ -0,0 +1,34 @@
+using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+namespace Basics
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+            Console.WriteLine($"a+b={a + b}");
+            Console.WriteLine($"a-b ={ a-b}");
+            Console.WriteLine($"a*b={a * b}");
+            Console.WriteLine($"a/b={a / b}");
+
+            float c = 20.0F;
+            Console.WriteLine($"c/b={c / b}");
+        }
+    }
+}
+
+a+b=30
+a-b =-10
+a*b=200
+a/b=0
+c/b=1
+
+C:\Users\SWETHAA\source\repos\Solution\Basics\bin\Debug\net8.0\Basics.exe (process 27968) exited with code 0 (0x0).
+To automatically close the console when debugging stops, enable Tools->Options->Debugging->Automatically close the console when debugging stops.
+Press any key to close this window . . .
