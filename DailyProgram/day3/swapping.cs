b0f153bff127fc7c3654a345d7291f94d273c9b6
Swethaa S P <97969029+swethaasp@users.noreply.github.com>
2024-08-29 14:06:29 +0530

    Update swapping.cs

--- DailyProgram/day3/swapping.cs
+++ DailyProgram/day3/swapping.cs
@@ -10,7 +10,36 @@ public class HelloWorld
         int temp=b;
         b=a;
         a=temp;
-        
+
+        /* Using Tuple Deconstruction
+        int a = 5;
+        int b = 10;
+        (a, b) = (b, a);
+
+        */
+
+        /*
+        int a = 5;
+        int b = 10;
+        a = a + b;
+         b = a - b;
+        a = a - b;
+        */
+
+       /*
+        int a = 5;
+       int b = 10;
+       a = a ^ b;
+       b = a ^ b;
+      a = a ^ b;
+        */
+
+        /*LINQ
+        int[] array = { 5, 10 };
+         (array[0], array[1]) = (array[1], array[0]);
+
+        */
+
         Console.WriteLine($"After swapping: the value of a is {a} and b is {b}");
     }
 }
