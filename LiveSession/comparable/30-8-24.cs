def55d6fd9c117f60ae46e751991eb7fe7db8780
Swethaa S P <97969029+swethaasp@users.noreply.github.com>
2024-09-02 09:08:57 +0530

    Create 30-8-24.cs

---
+++ LiveSession/comparable/30-8-24.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+
+public class Employee : IComparable
+{
+    public string EmpName { get; set; }
+    public double Salary { get; set; }
+
+    public Employee(string empName, double salary)
+    {
+        EmpName = empName;
+        Salary = salary;
+    }
+
+    public int CompareTo(object obj)
+    {
+        Employee other = obj as Employee;
+        return EmpName.CompareTo(other.EmpName);
+        //return Salary.CompareTo(other.Salary);
+    }
+
+    public override string ToString()
+    {
+        return $"{EmpName} with {Salary} salary";
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        ArrayList employees = new ArrayList();
+
+        
+        employees.Add(new Employee("Alice", 50000));
+        employees.Add(new Employee("Bob", 70000));
+        employees.Add(new Employee("Charlie", 60000));
+
+       
+        Console.WriteLine("Original Employee List:");
+        foreach (Employee emp in employees)
+        {
+            Console.WriteLine(emp);
+        }
+
+        
+        employees.Sort();
+
+        Console.WriteLine("\nSorted Employee List by Salary:");
+        foreach (Employee emp in employees)
+        {
+            Console.WriteLine(emp);
+        }
+    }
+}
