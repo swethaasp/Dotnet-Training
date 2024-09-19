4e9a03eafdfee39259e4607e7de857140b6e813e
Swethaa S P <97969029+swethaasp@users.noreply.github.com>
2024-09-19 17:13:07 +0530

    Create 35. Search Insert Position.cs

---
+++ DailyProgram/LC/35. Search Insert Position.cs
@@ -0,0 +1,47 @@
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+Example 2:
+
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+Example 3:
+
+Input: nums = [1,3,5,6], target = 7
+Output: 4
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-104 <= nums[i] <= 104
+nums contains distinct values sorted in ascending order.
+-104 <= target <= 104
+
+public class Solution {
+    public int SearchInsert(int[] nums, int target) {
+        
+        int l=0;
+        int r=nums.Length-1;
+        while(l<=r){
+            int m =l+(r-l)/2;
+            if(nums[m]==target){
+                return m;
+            }
+            else if(nums[m]<target){
+                l=m+1;
+            }
+            else{
+                r=m-1;
+            }
+        }
+        return l;
+    }
+}
