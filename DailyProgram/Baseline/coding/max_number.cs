b9306054d55a7ed41a71f8d3d652d53a52f25e83
Swethaa S P <97969029+swethaasp@users.noreply.github.com>
2024-09-19 10:59:50 +0530

    Update max_number.cs

--- DailyProgram/Baseline/coding/max_number.cs
+++ DailyProgram/Baseline/coding/max_number.cs
@@ -42,3 +42,40 @@ public class Solution
         Console.WriteLine(max_number(A, L));  // Output: "1246789"
     }
 }
+
+
+//using hashset
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution
+{
+    public static string max_number(string A, string L)
+    {
+       HashSet<char> ht =new HashSet<char>();
+       foreach(char c in L){
+           ht.Add(c);
+       }
+       
+       List<char> lt =new List<char>();
+       foreach(char c1 in A){
+           if(!ht.Contains(c1)){
+            lt.Add(c1);   
+           }
+       }
+       
+      string s =new string(lt.ToArray());
+      return s;
+    }
+
+    public static void Main()
+    {
+        // Example input
+        string A = "123456789";
+        string L = "35";
+        
+        // Call the function and print the result
+        Console.WriteLine(max_number(A, L));  // Output: "1246789"
+    }
+}
