4c344d10d292cdfb681a1cd3e34eb33de6639d7e
Swethaa S P <97969029+swethaasp@users.noreply.github.com>
2024-09-22 11:19:09 +0530

    Create ReplicateVowel.cs

---
+++ DailyProgram/B-Module Test/debugging/ReplicateVowel.cs
@@ -0,0 +1,25 @@
+using System;
+
+class Solution
+{
+    static string ReplicateVowel(int n, string s)
+    {
+        string vowels = "aeiouAEIOU"; // Include uppercase vowels if needed
+        foreach(char c in s){
+           if(vowels.Contains(c)){   //remove ! 
+              return new string(c,n);
+           } 
+        }
+        return new string(s[0],n);
+        
+    }
+
+    static void Main(string[] args)
+    {
+        int n = Int32.Parse(Console.ReadLine().Trim());
+        string s = Console.ReadLine();
+
+        string result = ReplicateVowel(n, s);
+        Console.WriteLine(result);
+    }
+}
