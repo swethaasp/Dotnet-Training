ebbc85907bcb5e9c5f434b1802f4e0c35e5c076e
Swethaa S P <97969029+swethaasp@users.noreply.github.com>
2024-09-22 10:01:20 +0530

    Create PhoneNumber.cs

---
+++ DailyProgram/B-Module Test/coding/PhoneNumber.cs
@@ -0,0 +1,29 @@
+using System;
+
+class Solution
+{
+    // Function to extract the last 10 digits from the given string
+    static string PhoneNumber(string S)
+    {
+        string digits="";
+        foreach(char c in S){
+            if(char.IsDigit(c)){
+                digits+=c;
+            }
+            
+        }
+        return digits.Substring(digits.Length-10);
+
+      /*string digit=new string(S.Where(char.IsDigit).ToArray());
+        return digit.Substring(digit.Length-10);
+      */
+    }
+
+    static void Main(string[] args)
+    {
+        string S = Console.ReadLine();
+        string result = PhoneNumber(S);
+        Console.WriteLine(result);
+    }
+}
+
