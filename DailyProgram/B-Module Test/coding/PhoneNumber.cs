0c928c341638be7ab0f9ab380a48fada7a79a14b
Swethaa S P <97969029+swethaasp@users.noreply.github.com>
2024-09-24 13:32:17 +0530

    Update PhoneNumber.cs

--- DailyProgram/B-Module Test/coding/PhoneNumber.cs
+++ DailyProgram/B-Module Test/coding/PhoneNumber.cs
@@ -5,6 +5,11 @@ class Solution
     // Function to extract the last 10 digits from the given string
     static string PhoneNumber(string S)
     {
+
+        string digit=new string(S.Where(char.IsDigit).ToArray());
+        return digit.Substring(digit.Length-10);
+
+        //alt approach
         string digits="";
         foreach(char c in S){
             if(char.IsDigit(c)){
@@ -14,9 +19,7 @@ class Solution
         }
         return digits.Substring(digits.Length-10);
 
-      /*string digit=new string(S.Where(char.IsDigit).ToArray());
-        return digit.Substring(digit.Length-10);
-      */
+      
     }
 
     static void Main(string[] args)
